32211ff54c06730514f2ad8ce1738f96b9f16f49
Lauro Junior <laurossjunior@gmail.com>
2024-08-27 10:34:44 -0300

    Limpeza de linhas desnecessárias

--- Binary_search.rb
+++ Binary_search.rb
@@ -1,5 +1,5 @@
 array =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-tgt=4 #Tgt=Alvo
+tgt=15 #Tgt=Alvo
 alto=array.length-1 #Indice mais alto
 baixo=0 #Indice mais baixo
 meio=(alto-baixo)/2 #Indice do meio
@@ -9,10 +9,6 @@ while tgt!=array[meio]
   if tgt < array[meio]
     alto=meio-1
     meio=(alto+baixo)/2
-    #if tgt > array[meio]
-     # baixo=meio
-     # meio=alto
-   #end
   end
 
   if tgt > array[meio]
@@ -21,7 +17,7 @@ while tgt!=array[meio]
   end
  puts "Valor do meio=#{array[meio]} e posição=#{meio}"
 end
-puts "O valor #{tgt} está na posição #{meio} do array"
+puts "O valor alvo #{tgt} está na posição #{meio} do array"
 
 
 
